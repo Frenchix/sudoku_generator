919043061e534787675eed788f969f04e63359cc
Frenchix <bapf62@hotmail.com>
2021-09-05 20:09:39 +0200

    Draw Cell container

--- js/app.js
+++ js/app.js
@@ -1,18 +1,25 @@
 const app = {
     GATE: 9,
     init: function() {
-        app.drawGate();
+        app.drawCellContainer();
     },
     drawGate: function() {
         const gateDOM = document.getElementById("gate");
-        for (const cell = 0; cell < app.GATE; cell++) {
-            for (const row = 0; row < app.GATE; row++) {
-
+            for (var row = 0; row < app.GATE; row++) {
+                for (var cell = 0; cell < app.GATE; cell++) {
+                    const div = document.createElement("div");
+                    div.className = "cell";
+                    gateDOM.appendChild(div);
             }
         }
-        const div = document.createElement("div");
-        div.className = "cell";
-        gateDOM.appendChild(div);
+    },
+    drawCellContainer : function() {
+        const gateDOM = document.getElementById("gate");
+            for (var container = 0; container < app.GATE; container++) {
+                const div = document.createElement("div");
+                div.className = "cellContainer";
+                gateDOM.appendChild(div);
+        }
     },
 }
 
